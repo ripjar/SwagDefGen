3424347fc2c550d40461ece076d3624016bf53b0
Sik So <sik.so@ripjar.com>
2022-09-29 17:02:51 +0100

    read and write to file

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,10 @@
 import yaml from "js-yaml";
 import anyConversion from "./anyConversion";
 import { ConvertOptions } from "./interface/ConvertOptions";
+import fs from "fs";
 
-const obj = {
-};
-
+const filename = "a";
+const data = fs.readFileSync(`./src/${filename}.json`, "utf8");
 const options: ConvertOptions = {};
-console.log(yaml.dump(anyConversion(obj, options)));
+const result = yaml.dump(anyConversion(JSON.parse(data), options));
+fs.writeFileSync(`./src/${filename}.yaml`, result);
